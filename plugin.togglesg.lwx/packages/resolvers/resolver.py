b9cbc3897ce661d8f96936be4c3df51455329610
lwx <l@w.x>
2020-06-06 21:26:33 +0800

    Changed endpoint to firestore.

--- plugin.togglesg.lwx/packages/resolvers/resolver.py
+++ plugin.togglesg.lwx/packages/resolvers/resolver.py
@@ -1,16 +1,16 @@
 import requests
 
 def getAllSeries():
-    url = 'http://localhost:5000'
+    url = 'https://us-central1-togglesg.cloudfunctions.net/toggle'
     r = requests.get(url)
     content = r.json()
     content = content['shows']
     return content
 
 
-def getAllEpisodesOfSeries(seriesURL):
-    url = 'http://localhost:5000/show?showURL={}'.format(seriesURL)
-    r = requests.get(url)
-    # will change to array of objects, right now is array of links
-    episodes = r.json()
-    return episodes
+# def getAllEpisodesOfSeries(seriesURL):
+#     url = 'https://us-central1-togglesg.cloudfunctions.net/toggle?showURL={}'.format(seriesURL)
+#     r = requests.get(url)
+#     # will change to array of objects, right now is array of links
+#     episodes = r.json()
+#     return episodes
